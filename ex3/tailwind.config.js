417e662cbf425a1cfacbf140874e4229f467d565
carlson <sambacarlson@gmail.com>
2022-11-15 16:09:17 +0100

    initial commit. navbar structured

--- ex3/tailwind.config.js
+++ ex3/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },
+    extend: {
+      colors:{
+        es_green:'#3F6745',
+        es_orange: '#FD661F',
+        es_dark_green: '#182A1C',
+        es_gray: '#F4F3EF',
+        es_fair_pink: '#FBD0C0',
+        es_dark:'#0B120B',
+      }
+    },
   },
   plugins: [],
 }

c20db9f31d33b2c668417189a9883e78568b171d
carlson <sambacarlson@gmail.com>
2022-12-19 14:47:59 +0100

    Inintial commit of ex7

---
+++ ex7/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-2xl text-purple-400">Doing well</h2>
+    </div>
+  );
+}
+
+export default App;
